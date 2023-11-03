dfa00d2562a94dc51b5efc0d76d85ca0c479f9a9
Grant <grant@grants-mbp.main.local.una.edu>
2023-11-03 11:17:16 -0500

    fixed tabs and add menu items

---
+++ add.php
@@ -0,0 +1,38 @@
+<?php
+require('heading.php');
+// if submitted add an ev else display form
+require('footing.php');
+if(isset(_$_POST['addButton']))
+	addEV()
+else
+	displayForm();
+function displayForm() {
+	echo <<< FORM
+	<form action="add.php" method="post"></form>
+	<table>
+	<tr>
+		<th><label for="name">Model: </label></th>
+		<th><label for="year">Years: </label></th>
+		<th><label for="miles">Miles: </label></th>
+	</tr>
+	<tr>
+		<td><input type="text" name="name" id="name" required maxlength="64" placeholder="name of EV" autocomplete="off"></td>
+		<td><input type="text" name="year" id="year" required maxlength="9" placeholder="1970-2000" pattern="/d{4}-$|^\d{4}-\d{4}$" autocomplete="off"></td>
+		<td><input type="text" name="range" id="range" required maxlength="5" placeholder="999" pattern="^\d{1,5}$" autocomplete="off"></td>
+	</tr>
+
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td style="float:right;"> <input type="submit" name="addButton" value="Add EV"></td>
+	</tr>
+	</table>
+	</form>
+
+FORM;
+
+}
+
+function addEV() {
+
+}
---
+++ menu.php
@@ -0,0 +1,22 @@
+<?php
+
+require('heading.php');
+echo <<< BLOCKY
+	<table>
+	  <tr>
+	<td> &bullet; <a href="listing.php">Display EV list</a></td>
+	</tr>
+	  <tr>
+	<td> &bullet; <a href="add.php">Add Ev</a></td>
+	</tr>
+	  <tr>
+	<td> &bulltet; <a href="update.php">Update EV List</a></td>
+	</tr>
+
+	  <tr>
+	<td> &bulltet; <a href="delete.php">Delete EV</a></td>
+	</tr>
+</table>
+BLOCKY;
+
+require('footing.php');
