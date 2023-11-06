c2311a437303c6323d9f1fda2a8027a34e3d86d3
Grant <grant@grants-mbp.main.local.una.edu>
2023-11-06 12:10:52 -0600

    form

--- add.php
+++ add.php
@@ -9,7 +9,7 @@ if (isset($_POST['addButton'])) {
 }
 function displayForm() {
 	echo <<< FORM
-	<form action="add.php" method="post"></form>
+	<form action="add.php" method="post">
 	<table>
 	<tr>
 		<th><label for="name">Model: </label></th>
