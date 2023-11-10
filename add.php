29ea5cf8b5c55de03ee9f0c6652fb33ae444b10f
Grant <grant@Grants-MacBook-Pro.local>
2023-11-09 19:03:48 -0600

    add works

--- add.php
+++ add.php
@@ -18,7 +18,7 @@ function displayForm() {
 	</tr>
 	<tr>
 		<td><input type="text" name="name" id="name" required maxlength="64" placeholder="name of EV" autocomplete="off"></td>
-		<td><input type="text" name="year" id="year" required maxlength="9" placeholder="1970-2000" pattern="^\d{4}(-\d{4})?$" autocomplete="off"></td>
+		<td><input type="text" name="year" id="year" required maxlength="9" placeholder="1970-2000" autocomplete="off"></td>
 		<td><input type="text" name="range" id="range" required maxlength="5" placeholder="999" pattern="^\d{1,5}$" autocomplete="off"></td>
 	</tr>
 	<tr>
@@ -39,10 +39,12 @@ function addEV() {
 	$name = trim($name);
 	$name = filter_var($name, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^[0-9a-zA-Z !-\.]{1,64}$/")));
 	$years = trim($years);
-	$years = filter_var($years, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "^\d{4}(-\d{4})?$")));
+	$years = filter_var($years, FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => "/^\d{4}(-\d{4})?$/")));
 	$range = trim($range);
 	$range = filter_var($range, FILTER_VALIDATE_INT, array('options' => array('minval' => "1", "max_val" => "99999")));
-	if ($name != false && $years != false && $range != false) {
+	if ($name != false || $years != false || $range != false) {
+		die("$name, $years, $range");
+	} else {
 		// connect to database
 		require("credentials.php");
 		$db = mysqli_connect($hostname, $username, $password, $database);
@@ -50,7 +52,8 @@ function addEV() {
 		if (mysqli_connect_errno()) {
 			die("unable to connect to databse" . mysqli_connect_error());
 		}
-		$query = "INSERT INTO cars (name, productionYears, miles) VALUES('" . $name . "','" . $years . "','" . $range . "')";
+		$query = "INSERT INTO cars (name, productionYears, miles)
+		    VALUES('" . $name . "','" . $years . "','" . $range . "')";
 		if (mysqli_query($db, $query)) {
 			echo <<< SUCCESS
 			<div class="center">
@@ -64,7 +67,5 @@ SUCCESS;
 			</div>
 FAIL;
 		}
-	} else {
-		die("invalid inputs");
 	}
 }
