dea392d2b3715235148a3dc2bed5990becd6f5ab
hrantgartley <doyouevenjump@gmail.com>
2023-11-12 19:16:32 -0600

    dropdown_root.php to add relative files based on index location and keep normal dropdown.php for all other endpoints

---
+++ includes/dropdown_root.php
@@ -0,0 +1,12 @@
+<div class="menu-btn">&#9776;</div>
+<nav class="menu">
+    <ul>
+        <div id="close-icon" style=" padding: .5em;">close &#x2717;</div>
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="./includes/listing.php">Display</a></li>
+        <li><a href="./includes/add.php">Add</a></li>
+        <li><a href="./includes/delete.php">Delete</a></li>
+        <li><a href="./includes/update.php">Update</a></li>
+    </ul>
+</nav>
+<script src="../js/script.js"></script>
--- index.php
+++ index.php
@@ -2,27 +2,29 @@
 <html lang="en">
 
 <head>
-	<title></title>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link type="icon" href="./images/carFav.ico" rel="icon">
+    <title></title>
+    <meta charset="UTF-8" name="Grant" content="Car Management System" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link type="icon" href="./images/carFav.ico" rel="icon">
+    <link rel="stylesheet" href="css/list.css">
+    <link rel="stylesheet" href="css/menu.css">
 </head>
 
 <body>
-	<a href="./includes/menu.php">Car Entry System</a>
-	<style>
-		a {
-			text-align: center;
-			font-size: 28px;
-			padding-left: 25em;
-		}
+    <?php require('./includes/dropdown_root.php') ?>
+    <script src="js/script.js"></script>
+    <style>
+        a {
+            text-align: center;
+            font-size: 28px;
+            padding-left: 25em;
+        }
 
-		body {
-
-			background-image: url(./images/background.webp);
-			max-width: 100%;
-		}
-	</style>
+        body {
+            background-image: url(./images/background.webp);
+            max-width: 100%;
+        }
+    </style>
 </body>
 
 </html>
