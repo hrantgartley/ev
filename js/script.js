b61cbbe30c1f393be46c7bcce6e23c0ec697ae1b
hrantgartley <doyouevenjump@gmail.com>
2023-11-13 10:16:24 -0600

    changes anon functions to arrow functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     let menuButton = document.querySelector(".menu-btn");
     let menu = document.querySelector(".menu");
     let menuLinks = document.querySelectorAll(".menu a");
     let close = document.querySelector("#close-icon");
 
-    function toggleMenu() {
+    const toggleMenu = () => {
         menu.style.display = menu.style.display === "block" ? "none" : "block";
-    }
+    };
 
     menuButton.addEventListener("click", toggleMenu);
     close.addEventListener("click", toggleMenu);
 
-    menuLinks.forEach(function (link) {
-        link.addEventListener("click", function () {
+    menuLinks.forEach((link) => {
+        link.addEventListener("click", () => {
             toggleMenu();
         });
     });
