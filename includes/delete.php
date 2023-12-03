9c35b55c10a5d25e8bcb82dd1e3f5d7bd710ddde
hrantgartley <doyouevenjump@gmail.com>
2023-12-02 20:24:14 -0600

    FINISHED FOR THE SEMESTER

--- includes/delete.php
+++ includes/delete.php
@@ -3,96 +3,98 @@
 require('heading.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_ids'])) {
-	handleDelete();
+    handleDelete();
 }
 
 displayList();
 require('footing.php');
 
 function handleDelete() {
-	require('credentials.php');
-	$db = mysqli_connect($hostname, $username, $password, $database);
+    require('credentials.php');
+    $db = mysqli_connect($hostname, $username, $password, $database);
 
-	if ($db === false) {
-		die("Unable to connect: " . mysqli_connect_error());
-	}
+    if ($db === false) {
+        die("Unable to connect: " . mysqli_connect_error());
+    }
 
-	$useDB = mysqli_select_db($db, 'ev');
-	if (!$useDB) {
-		die("Unable to select db: " . mysqli_error($db));
-	}
+    $useDB = mysqli_select_db($db, 'ev');
+    if (!$useDB) {
+        die("Unable to select db: " . mysqli_error($db));
+    }
 
-	foreach ($_POST['delete_ids'] as $id) {
-		$id = mysqli_real_escape_string($db, $id);
-		$result = mysqli_query($db, "DELETE FROM cars WHERE id = '$id'");
+    foreach ($_POST['delete_ids'] as $id) {
+        $id = mysqli_real_escape_string($db, $id);
+        $result = mysqli_query($db, "DELETE FROM members WHERE id = '$id'");
 
-		if ($result === false) {
-			die("Delete query failed: " . mysqli_error($db));
-		}
-	}
+        if ($result === false) {
+            die("Delete query failed: " . mysqli_error($db));
+        }
+    }
 
-	mysqli_close($db);
+    mysqli_close($db);
 }
 
 function displayList() {
-	$background = 0;
-	echo <<< BLOCK
+    $background = 0;
+    echo <<< BLOCK
     <div style="text-align: center;">
         <form method="post" action="">
             <table style="margin-left: auto; margin-right: auto; width: 50%">
                 <tr>
                     <th>Delete</th>
                     <th>Name</th>
-                    <th>Production Years</th>
-                    <th>Range</th>
+                    <th>Email</th>
+                    <th>Expires</th>
                 </tr>
 BLOCK;
 
-	require('credentials.php');
-	$db = mysqli_connect($hostname, $username, $password, $database);
+    require('credentials.php');
+    $db = mysqli_connect($hostname, $username, $password, $database);
 
-	if ($db === false) {
-		die("Unable to connect: " . mysqli_connect_error());
-	}
+    if ($db === false) {
+        die("Unable to connect: " . mysqli_connect_error());
+    }
 
-	$useDB = mysqli_select_db($db, 'ev');
-	if (!$useDB) {
-		die("Unable to select db: " . mysqli_error($db));
-		echo "            \r</table>";
-	}
+    $useDB = mysqli_select_db($db, 'namb');
+    if (!$useDB) {
+        die("Unable to select db: " . mysqli_error($db));
+        echo "            \r</table>";
+    }
 
-	$cars = mysqli_query($db, "SELECT id, name, productionYears, miles FROM cars ORDER BY productionYears");
-	if ($cars === false) {
-		die("Query failed: " . mysqli_error($db));
-	}
+    $members = mysqli_query($db, "SELECT id, name, email, expires FROM members ORDER BY name");
+    if ($members === false) {
+        die("Query failed: " . mysqli_error($db));
+    }
 
-	while ($row = mysqli_fetch_array($cars)) {
-		$id = $row[0];
-		$name = $row[1];
-		$productionYears = $row[2];
-		$miles = $row[3];
+    while ($row = mysqli_fetch_array($members)) {
+        $id = $row['id'];
+        $name = $row['name'];
+        $email = $row['email'];
+        $expires = $row['expires'];
 
-		if ($background++ % 2 == 0) {
-			echo "        <tr style=\"background-color: white\">\n";
-		} else {
-			echo "        <tr style=\"background-color: lightgrey\">\n";
-		}
+        if ($background++ % 2 == 0) {
+            echo "        <tr style=\"background-color: white\">\n";
+        } else {
+            echo "        <tr style=\"background-color: lightgrey\">\n";
+        }
 
-		echo <<< TABLE
+        echo <<< TABLE
         <td><input type="checkbox" name="delete_ids[]" value="$id"></td>
         <td>$name</td>
-        <td>$productionYears</td>
-        <td>$miles</td>
+        <td>$email</td>
+        <td>$expires</td>
       </tr>
 TABLE;
-	}
+    }
 
-	echo <<< BUTTON
+    echo <<< BUTTON
             </table>
             <button type="submit" style="margin-right: 43%">Delete Selected</button>
         </form>
     </div>
 BUTTON;
 
-	mysqli_close($db);
+    mysqli_close($db);
 }
+?>
+
--- includes/update.php
+++ includes/update.php
@@ -1,136 +1,111 @@
 <?php
-
 require('heading.php');
 
-// Check if the form for updating the car is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update_id'])) {
-	handleUpdate();
+// Check if the form for updating the member is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updateButton'])) {
+    handleUpdate();
 }
 
-// Check if the select menu for choosing the car is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selected_car'])) {
-	$selectedCarId = $_POST['selected_car'];
-	displayUpdateForm($selectedCarId);
+// Check if the select menu for choosing the member is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['selected_member'])) {
+    $selectedMemberId = $_POST['selected_member'];
+    displayUpdateForm($selectedMemberId);
 } else {
-	displaySelectMenu();
+    displaySelectMenu();
 }
 
 require('footing.php');
 
 function handleUpdate() {
-	require('credentials.php');
-	$db = mysqli_connect($hostname, $username, $password, $database);
+    require('credentials.php');
+    $db = mysqli_connect($hostname, $username, $password, $database);
 
-	if ($db === false) {
-		die("Unable to connect: " . mysqli_connect_error());
-	}
+    if ($db === false) {
+        die("Unable to connect: " . mysqli_connect_error());
+    }
 
-	$useDB = mysqli_select_db($db, 'namb');
-	if (!$useDB) {
-		die("Unable to select db: " . mysqli_error($db));
-	}
+    $useDB = mysqli_select_db($db, 'namb');
+    if (!$useDB) {
+        die("Unable to select db: " . mysqli_error($db));
+    }
 
-	$id = $_POST['update_id'];
-	$name = mysqli_real_escape_string($db, $_POST['name_var']);
-	$productionYears = mysqli_real_escape_string($db, $_POST['year_var']);
-	$miles = mysqli_real_escape_string($db, $_POST['range_var']);
+    $id = $_POST['update_id'];
+    $name = mysqli_real_escape_string($db, $_POST['name_var']);
+    $email = mysqli_real_escape_string($db, $_POST['email_var']);
+    $expires = mysqli_real_escape_string($db, $_POST['expires_var']);
 
-	$result = mysqli_query($db, "UPDATE cars SET name='$name', productionYears='$productionYears', miles='$miles' WHERE id='$id'");
+    $query = "UPDATE members SET name='$name', email='$email', expires='$expires' WHERE ID='$id'";
+    $result = mysqli_query($db, $query);
 
-	if ($result === false) {
-		die("Update query failed: " . mysqli_error($db));
-	}
+    if ($result === false) {
+        die("Update query failed: " . mysqli_error($db));
+    }
 
-	mysqli_close($db);
-	sleep(1);
-	header("Location: listing.php");
-	exit();
+    mysqli_close($db);
+    header("Location: listing.php");
+    exit();
 }
 
 function displaySelectMenu() {
-	require('credentials.php');
-	$db = mysqli_connect($hostname, $username, $password, $database);
+    require('credentials.php');
+    $db = mysqli_connect($hostname, $username, $password, $database);
 
-	if ($db === false) {
-		die("Unable to connect: " . mysqli_connect_error());
-	}
-
-	$useDB = mysqli_select_db($db, 'namb');
-	if (!$useDB) {
-		die("Unable to select db: " . mysqli_error($db));
-	}
-
-	$cars = mysqli_query($db, "SELECT ID,name FROM members ORDER BY name");
-	if ($cars === false) {
-		die("Query failed: " . mysqli_error($db));
-	}
-	echo <<< BEFOREFORM
-	<form
-		method="post"
-		action=""
-		style="
-			margin-left: auto;
-			margin-right: auto;
-			width: 50%;
-			padding-left: 30%;
-			font-size: 20px;
-		"
-	>
-		<label for="selected_car" style="font-size: 20px"
-			>Select a car to update:</label
-		><br />
-		<select name="selected_car" id="selected_car">
-	
-BEFOREFORM;
-	while ($row = mysqli_fetch_array($cars)) {
-		$id = $row['id'];
-		$name = $row['name'];
-		echo "<option value='$id'>$name</option>";
-	}
-	echo <<< AFTERFORM
-    </select><br>
-		<input type="submit" value="Update Car">
-    </form>
-AFTERFORM;
-
-	mysqli_close($db);
-}
-
-function displayForm($cars)
-{
-    echo <<< FORM
-    <form action="update.php" method="post">
-    <table style="margin-right: auto; margin-left: auto; width: 50%" id="updateTable">
-    <tr>
-        <th><label for="car">Select Car: </label></th>
-        <td>
-            <select name="car" id="car" required>
-                <option value="">Select a Car</option>
-FORM;
-
-    // Loop through the cars array to populate the options
-    foreach ($cars as $car) {
-        $name = $car['name'];
-        $productionYears = $car['productionYears'];
-        $miles = $car['miles'];
-
-        echo "<option value=\"$name\">$name - $productionYears - $miles miles</option>";
+    if ($db === false) {
+        die("Unable to connect: " . mysqli_connect_error());
     }
 
-    echo <<< FORM
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <th><label for="newMiles">New Miles: </label></th>
-        <td><input type="text" name="newMiles" id="newMiles" required maxlength="5" placeholder="New Miles" pattern="^\d{1,5}$" autocomplete="off"></td>
-    </tr>
-    <tr>
-        <td>&nbsp;</td>
-        <td style="float:right;"> <input type="submit" name="updateButton" value="Update"></td>
-    </tr>
-    </table>
-    </form>
-FORM;
+    $useDB = mysqli_select_db($db, 'namb');
+    if (!$useDB) {
+        die("Unable to select db: " . mysqli_error($db));
+    }
+
+    $members = mysqli_query($db, "SELECT ID, name FROM members ORDER BY name");
+    if ($members === false) {
+        die("Query failed: " . mysqli_error($db));
+    }
+
+    echo '<form method="post" action=""><label for="selected_member">Select a member to update:</label><br />';
+    echo '<select name="selected_member" id="selected_member">';
+
+    while ($row = mysqli_fetch_array($members)) {
+        $id = $row['ID'];
+        $name = $row['name'];
+        echo "<option value='$id'>$name</option>";
+    }
+
+    echo '</select><br /><input type="submit" value="Update Member"></form>';
+
+    mysqli_close($db);
 }
+
+function displayUpdateForm($selectedMemberId) {
+    require('credentials.php');
+    $db = mysqli_connect($hostname, $username, $password, $database);
+
+    if ($db === false) {
+        die("Unable to connect: " . mysqli_connect_error());
+    }
+
+    $useDB = mysqli_select_db($db, 'namb');
+    if (!$useDB) {
+        die("Unable to select db: " . mysqli_error($db));
+    }
+
+    $query = "SELECT name, email, expires FROM members WHERE ID=?";
+    $stmt = mysqli_prepare($db, $query);
+    mysqli_stmt_bind_param($stmt, "i", $selectedMemberId);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $name, $email, $expires);
+    mysqli_stmt_fetch($stmt);
+
+    echo '<form method="post" action="" class="center"><input type="hidden" name="update_id" value="' . $selectedMemberId . '">';
+    echo 'Name: <input type="text" name="name_var" value="' . $name . '"><br />';
+    echo 'Email: <input type="text" name="email_var" value="' . $email . '"><br />';
+    echo 'Expires: <input type="text" name="expires_var" value="' . $expires . '"><br />';
+    echo '<input type="submit" name="updateButton" value="Update"></form>';
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($db);
+}
+?>
 
